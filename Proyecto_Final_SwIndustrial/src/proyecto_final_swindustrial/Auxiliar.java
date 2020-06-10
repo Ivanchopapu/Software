00f74be9692baf874e6c8e1f91491ae6be79d261
Ivanchopapu <66742762+Ivanchopapu@users.noreply.github.com>
2020-06-10 12:31:48 -0500

    Update Auxiliar.java

--- Proyecto_Final_SwIndustrial/src/proyecto_final_swindustrial/Auxiliar.java
+++ Proyecto_Final_SwIndustrial/src/proyecto_final_swindustrial/Auxiliar.java
@@ -4,26 +4,5 @@ package proyecto_final_swindustrial;
 
 public class Auxiliar {
     
-    int x;
-    int y;
-    String A = "Chale";
-    
-    
-    
-    public void putear()
-    {
-        System.out.println(" Puto el que lo lea " + A);
-        
-    }
-    public void chalemasivo()
-    {
-        int x = 0;
-        
-        while(x<20)
-        {
-            System.out.println("Putos todos x " + x);
-        }
-        x++;
-    }
     
 }
