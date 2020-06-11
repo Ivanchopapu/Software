70efb9fbec94f87a9eb48ce3931920108490ec50
Tofiter <christopheradan9@gmail.com>
2020-06-10 19:02:17 -0500

    Empleado hecho

--- Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Empleado.java
+++ Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Empleado.java
@@ -12,8 +12,73 @@ package com.idesi.proyecto;
 public class Empleado extends Persona{
     private int codigoEmpleado;
     private String tipoEmpleado;
-    private double salario;
     private String RFC;
+    private double salario;
+    
+    public Empleado(){}
+
+    public Empleado(String nombre, int edad, int codigoEmpleado, String tipoEmpleado, String RFC, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.codigoEmpleado = codigoEmpleado;
+        this.tipoEmpleado = tipoEmpleado;
+        this.RFC = RFC;
+        this.salario = salario;
+    }
+
+    public int getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(int codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public String getTipoEmpleado() {
+        return tipoEmpleado;
+    }
+
+    public void setTipoEmpleado(String tipoEmpleado) {
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+    
+    
     
     
     
