561a1aab4ae7d6be91e8e4d30666ac57ec522fde
Ivanchopapu <66742762+Ivanchopapu@users.noreply.github.com>
2020-06-10 19:06:33 -0500

    Update Empleado.java

--- Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Empleado.java
+++ Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Empleado.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.idesi.proyecto;
 
-/**
- *
- * @author Cristofer
- */
 public class Empleado extends Persona{
     private int codigoEmpleado;
     private String tipoEmpleado;
