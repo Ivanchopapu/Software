fd60aa02e28ca07fb7bde526e7ccd5281db7e3f5
Tofiter <christopheradan9@gmail.com>
2020-06-10 19:24:16 -0500

    Producto hecho

--- Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Producto.java
+++ Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/Producto.java
@@ -10,5 +10,62 @@ package com.idesi.proyecto;
  * @author Cristofer
  */
 public class Producto {
+    private int codigo;
+    private String tipoProducto;
+    private String nombreProducto;
+    private int existencia;
+    private int precio;
+    
+    public Producto(){}
+
+    public Producto(int codigo, String tipoProducto, String nombreProducto, int existencia, int precio) {
+        this.codigo = codigo;
+        this.tipoProducto = tipoProducto;
+        this.nombreProducto = nombreProducto;
+        this.existencia = existencia;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public void setTipoProducto(String tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
     
 }
