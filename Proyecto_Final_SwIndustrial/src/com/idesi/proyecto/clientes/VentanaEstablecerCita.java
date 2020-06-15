ad08ea3fda721e33d2010781644ceb578a7a9b50
Tofiter <christopheradan9@gmail.com>
2020-06-15 14:12:57 -0500

    Cambios minimos en Cliente

--- Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/clientes/VentanaEstablecerCita.java
+++ Proyecto_Final_SwIndustrial/src/com/idesi/proyecto/clientes/VentanaEstablecerCita.java
@@ -144,7 +144,7 @@ public class VentanaEstablecerCita extends javax.swing.JFrame {
 
         cmbTipoExamen.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Sangre", "Nivel de Azucar", "Orina" }));
 
-        lblFecha.setText("Fecha (DD/MM:");
+        lblFecha.setText("Fecha (DD/MM):");
 
         cmbDia.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"
             , "25", "26", "27", "28", "29", "30", "31", }));
